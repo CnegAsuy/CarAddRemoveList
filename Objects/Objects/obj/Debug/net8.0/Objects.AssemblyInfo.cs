902c3d5ce379d898d5604c3fb821b41f19e432b0
CnegAsuy <81309318+CnegAsuy@users.noreply.github.com>
2024-12-14 02:12:19 +0300

    wha-

--- Objects/Objects/obj/Debug/net8.0/Objects.AssemblyInfo.cs
+++ Objects/Objects/obj/Debug/net8.0/Objects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Objects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffabdba9a1a7e01d5cbdc9e141135cdbe9dfece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca50a1a33ed286536fa210393006967604ad9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Objects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Objects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
